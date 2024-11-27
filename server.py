6697156f1ea808cb7a04dd8c41af85d2658cd21a
Deni Gunawan <denigunawan775@gmail.com>
2024-11-27 08:23:35 +0700

    update

--- server.py
+++ server.py
@@ -12,7 +12,18 @@ def index():
 
 @app.route('/ant-algorithm-analytic')
 def data():
-    return render_template("detail.html")
+    table_data = [
+        {"name": "Design Website", "distance_x": 20, "distance_y": 15, "goods_received": 50, 
+         "estimated_time": "1h", "lead_time": "2h"},
+         
+        {"name": "Develop Backend", "distance_x_cm": 25, "distance_y_cm": 20, "goods_received_kg": 60, 
+         "estimated_time": "2h", "lead_time": "3h"},
+    ]
+    return render_template("ant.html", table_data=table_data)
+
+@app.route('/ant-algorithm-analytic/proses-ant')
+def ant():
+    return render_template("proses-ant.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
